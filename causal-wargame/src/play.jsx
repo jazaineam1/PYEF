273cb8433cdb213af7d18701cf50ff47fb244ac9
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 01:13:02 -0500

    ux: load v5 role chart styles in participant view

--- causal-wargame/src/play.jsx
+++ causal-wargame/src/play.jsx
@@ -6,6 +6,7 @@ import './quest-v4.css'
 import './quest-v41.css'
 import './quest-v5.css'
 import './quest-v5-extra.css'
+import './quest-v5-visuals.css'
 import ModePlay from '#play'
 
 createRoot(document.getElementById('root')).render(<ModePlay/>)
