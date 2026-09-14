c20db5ec6b6f4c39b9c82bd98284a6d6903b52de
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 00:11:27 -0500

    feat: load quest v4 facilitator UX

--- causal-wargame/src/facilitator.jsx
+++ causal-wargame/src/facilitator.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './quest.css'
+import './quest-v4.css'
 import ModeFacilitator from '#facilitator'
 
 createRoot(document.getElementById('root')).render(<ModeFacilitator/>)
