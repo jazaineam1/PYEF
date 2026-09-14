c7a4855740366ee3d77baaa01ea2a5e67f1b7c57
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 01:11:26 -0500

    ui: load v5 lightweight visual layer on wall

--- causal-wargame/src/wall.jsx
+++ causal-wargame/src/wall.jsx
@@ -4,6 +4,7 @@ import './styles.css'
 import './quest.css'
 import './quest-v4.css'
 import './quest-v5.css'
+import './quest-v5-extra.css'
 import ModeWall from '#wall'
 
 createRoot(document.getElementById('root')).render(<ModeWall/>)
