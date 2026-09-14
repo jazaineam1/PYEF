e65337a061de5fc501fbc63b53a52a29f61a84d8
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 00:11:22 -0500

    feat: load quest v4 participant UX

--- causal-wargame/src/play.jsx
+++ causal-wargame/src/play.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './quest.css'
+import './quest-v4.css'
 import ModePlay from '#play'
 
 createRoot(document.getElementById('root')).render(<ModePlay/>)
