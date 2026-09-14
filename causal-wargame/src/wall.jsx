b27a2ebc2da8e316cc7251770a7b221981e5985c
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-13 23:52:50 -0500

    style: load Causal Quest visual system in wall view

--- causal-wargame/src/wall.jsx
+++ causal-wargame/src/wall.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './quest.css'
 import ModeWall from '#wall'
 
 createRoot(document.getElementById('root')).render(<ModeWall/>)
