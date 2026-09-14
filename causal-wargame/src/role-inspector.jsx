bddc9493d56261341c1f831023dfa71e1ddcf033
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 11:47:09 -0500

    feat: add role inspector entrypoint

---
+++ causal-wargame/src/role-inspector.jsx
@@ -0,0 +1,14 @@
+import React from'react'
+import{createRoot}from'react-dom/client'
+import'./styles.css'
+import'./quest.css'
+import'./quest-v4.css'
+import'./quest-v41.css'
+import'./quest-v5.css'
+import'./quest-v5-extra.css'
+import'./quest-v5-visuals.css'
+import'./role-visual.css'
+import'./quest-v6.css'
+import FacilitatorRoleInspector from'./pages/FacilitatorRoleInspector'
+
+createRoot(document.getElementById('root')).render(<FacilitatorRoleInspector/>)
