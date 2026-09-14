71ef8c5b9cbe4e8c787bfc7b2ff5eab461b602ee
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 00:49:45 -0500

    ui: load v5 participant theme

--- causal-wargame/src/play.jsx
+++ causal-wargame/src/play.jsx
@@ -4,6 +4,7 @@ import './styles.css'
 import './quest.css'
 import './quest-v4.css'
 import './quest-v41.css'
+import './quest-v5.css'
 import ModePlay from '#play'
 
 createRoot(document.getElementById('root')).render(<ModePlay/>)
