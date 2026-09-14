224094c45be30aedcbe6078ab6488d7504d6a6a1
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 00:11:34 -0500

    feat: load quest v4 wall UX

--- causal-wargame/src/wall.jsx
+++ causal-wargame/src/wall.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './quest.css'
+import './quest-v4.css'
 import ModeWall from '#wall'
 
 createRoot(document.getElementById('root')).render(<ModeWall/>)
