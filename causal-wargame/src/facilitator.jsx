48467739000b1d3f1097c626befdb8537a988bb4
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 00:49:57 -0500

    ui: load v5 facilitator theme

--- causal-wargame/src/facilitator.jsx
+++ causal-wargame/src/facilitator.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './quest.css'
 import './quest-v4.css'
+import './quest-v5.css'
 import ModeFacilitator from '#facilitator'
 
 createRoot(document.getElementById('root')).render(<ModeFacilitator/>)
