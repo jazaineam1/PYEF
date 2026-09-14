d11a3b981378931fe05007085ef0fa2181ee22a2
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 01:11:19 -0500

    ui: load v5 lightweight visual layer in facilitator

--- causal-wargame/src/facilitator.jsx
+++ causal-wargame/src/facilitator.jsx
@@ -4,6 +4,7 @@ import './styles.css'
 import './quest.css'
 import './quest-v4.css'
 import './quest-v5.css'
+import './quest-v5-extra.css'
 import ModeFacilitator from '#facilitator'
 
 createRoot(document.getElementById('root')).render(<ModeFacilitator/>)
