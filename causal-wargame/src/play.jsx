4d7d8f8f5ebc41792f463fd6c6776265c6a24e47
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 11:17:06 -0500

    chore: load emoji-first role visual styles

--- causal-wargame/src/play.jsx
+++ causal-wargame/src/play.jsx
@@ -7,7 +7,7 @@ import './quest-v41.css'
 import './quest-v5.css'
 import './quest-v5-extra.css'
 import './quest-v5-visuals.css'
-import './role-art.css'
+import './role-visual.css'
 import './quest-v6.css'
 import ModePlay from '#play'
 
