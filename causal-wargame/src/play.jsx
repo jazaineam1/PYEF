3b480e3ae8518774d454b90ea058ddc6336cb50a
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 00:19:41 -0500

    feat: load investment basket styles

--- causal-wargame/src/play.jsx
+++ causal-wargame/src/play.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './quest.css'
 import './quest-v4.css'
+import './quest-v41.css'
 import ModePlay from '#play'
 
 createRoot(document.getElementById('root')).render(<ModePlay/>)
