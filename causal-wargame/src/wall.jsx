9ef8b60503e9369d27d73509874fa7731e182775
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 00:50:03 -0500

    ui: load v5 wall theme

--- causal-wargame/src/wall.jsx
+++ causal-wargame/src/wall.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './quest.css'
 import './quest-v4.css'
+import './quest-v5.css'
 import ModeWall from '#wall'
 
 createRoot(document.getElementById('root')).render(<ModeWall/>)
