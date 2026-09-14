a7e15bf18f53950835ab9b091a346028374156e5
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-13 23:52:31 -0500

    style: load Causal Quest visual system in participant view

--- causal-wargame/src/play.jsx
+++ causal-wargame/src/play.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './quest.css'
 import ModePlay from '#play'
 
 createRoot(document.getElementById('root')).render(<ModePlay/>)
