6c64ffb77ed5574ef020fd702700b92cb51e806a
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-13 23:52:39 -0500

    style: load Causal Quest visual system in facilitator view

--- causal-wargame/src/facilitator.jsx
+++ causal-wargame/src/facilitator.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './quest.css'
 import ModeFacilitator from '#facilitator'
 
 createRoot(document.getElementById('root')).render(<ModeFacilitator/>)
