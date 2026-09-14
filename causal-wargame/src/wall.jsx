daacc91f0eb57748f9ba52219a169d774bda4ed7
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-14 01:17:00 -0500

    wall: load final score visualization styles

--- causal-wargame/src/wall.jsx
+++ causal-wargame/src/wall.jsx
@@ -5,6 +5,7 @@ import './quest.css'
 import './quest-v4.css'
 import './quest-v5.css'
 import './quest-v5-extra.css'
+import './wall-v5-extra.css'
 import ModeWall from '#wall'
 
 createRoot(document.getElementById('root')).render(<ModeWall/>)
