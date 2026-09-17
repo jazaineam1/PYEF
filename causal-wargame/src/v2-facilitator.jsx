e8ffa4adfe1276ce2d1fd35092232b9b57dc7df2
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-17 16:14:31 -0500

    feat(v2): add facilitator entrypoint

---
+++ causal-wargame/src/v2-facilitator.jsx
@@ -0,0 +1,5 @@
+import React from'react'
+import{createRoot}from'react-dom/client'
+import'./v2/styles.css'
+import V2Facilitator from'./v2/V2Facilitator.jsx'
+createRoot(document.getElementById('root')).render(<V2Facilitator/>)
